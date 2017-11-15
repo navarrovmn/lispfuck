a3350c77dff844e58ca73f8f92126d8423320371
Victor Navarro <victor.matias.navarro@gmail.com>
2017-11-15 18:07:14 -0200

    Started initial lexer and parser
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>

---
+++ lispfuck.py
@@ -0,0 +1,57 @@
+import ox
+
+op_to_desc = {
+    'read',
+    'left',
+    'inc',
+    'add',
+    'dec',
+    'print',
+    'read',
+    ''
+}
+
+treat_list = lambda term, other_term: (term, other_term)
+treat_atom = lambda op: (op_to_desc[op], op)
+
+lexer = ox.make_lexer([
+    ('OPENING_BLOCK', r'\('),
+    ('CLOSING_BLOCK', r'\)'),
+    ('RIGHT_TAPE', r'right'),
+    ('LEFT_TAPE', r'left'),
+    ('SUM', r'inc|add'),
+    ('SUB', r'dec'),
+    ('PRINT', r'print'),
+    ('READ', r'read'),
+    ('NUMBER', r'\d+(\.\d*)?'),
+    ('NAME', r'[a-zA-Z_][a-zA-Z_0-9]*'),
+])
+
+tokens = [
+    'NUMBER',
+    'OPENING_BLOCK',
+    'CLOSING_BLOCK',
+    'RIGHT_TAPE',
+    'LEFT_TAPE',
+    'SUM',
+    'SUB',
+    'PRINT',
+    'READ'
+]
+
+# parser = ox.make_parser([
+#     ('lisp_list: lisp_list atom', treat_list),
+#     ('lisp_list: atom', treat_atom),
+#     ('atom: RIGHT_TAPE', treat_atom),
+#     ('atom: LEFT_TAPE', treat_atom),
+#     ('atom: SUM', treat_atom),
+#     ('atom: SUB', treat_atom),
+#     ('atom: PRINT', treat_atom),
+#     ('atom: READ', treat_atom),
+#     ('atom: NUMBER', lambda number : ('simple_number', float(number))),
+#     ('atom: NAME', lambda name : ('simple_name', name)),
+# ], tokens)
+
+parser = ox.make_parser([
+    ('lisp_list: lisp_list atom', treat_list),
+], tokens)
