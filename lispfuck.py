ba7be38da5e6312c1bec5fe398490a516785c125
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-11-15 18:19:40 -0200

    Updating op_to_desc dict

--- lispfuck.py
+++ lispfuck.py
@@ -1,14 +1,13 @@
 import ox
 
 op_to_desc = {
-    'read',
-    'left',
-    'inc',
-    'add',
-    'dec',
-    'print',
-    'read',
-    ''
+    'read': 'input function',
+    'left': 'walk left',
+    'right': 'walk right',
+    'inc': 'increment tape',
+    'add': 'increment tape',
+    'dec': 'decrement tape',
+    'print': 'print value'
 }
 
 treat_list = lambda term, other_term: (term, other_term)
@@ -27,7 +26,7 @@ lexer = ox.make_lexer([
     ('NAME', r'[a-zA-Z_][a-zA-Z_0-9]*'),
 ])
 
-tokens = [
+tokens_list = [
     'NUMBER',
     'OPENING_BLOCK',
     'CLOSING_BLOCK',
@@ -39,19 +38,16 @@ tokens = [
     'READ'
 ]
 
-# parser = ox.make_parser([
-#     ('lisp_list: lisp_list atom', treat_list),
-#     ('lisp_list: atom', treat_atom),
-#     ('atom: RIGHT_TAPE', treat_atom),
-#     ('atom: LEFT_TAPE', treat_atom),
-#     ('atom: SUM', treat_atom),
-#     ('atom: SUB', treat_atom),
-#     ('atom: PRINT', treat_atom),
-#     ('atom: READ', treat_atom),
-#     ('atom: NUMBER', lambda number : ('simple_number', float(number))),
-#     ('atom: NAME', lambda name : ('simple_name', name)),
-# ], tokens)
-
 parser = ox.make_parser([
     ('lisp_list: lisp_list atom', treat_list),
-], tokens)
+    ('lisp_list: atom', treat_atom),
+    ('atom: RIGHT_TAPE', treat_atom),
+    ('atom: LEFT_TAPE', treat_atom),
+    ('atom: SUM', treat_atom),
+    ('atom: SUB', treat_atom),
+    ('atom: PRINT', treat_atom),
+    ('atom: READ', treat_atom),
+    ('atom: NUMBER', lambda number : ('simple_number', float(number))),
+    ('atom: NAME', lambda name : ('simple_name', name)),
+], tokens_list)
+
