caf95b62c53860de6015e084e76ceb53b6a72504
Victor Navarro <victor.matias.navarro@gmail.com>
2017-11-20 22:23:05 -0200

    Using simplefied approach to parser
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>

--- lispfuck.py
+++ lispfuck.py
@@ -1,27 +1,8 @@
 import ox
 
-op_to_desc = {
-    'read': 'input function',
-    'left': 'walk left',
-    'right': 'walk right',
-    'inc': 'increment tape',
-    'add': 'increment tape',
-    'dec': 'decrement tape',
-    'print': 'print value'
-}
-
-treat_list = lambda term, other_term: (term, other_term)
-treat_atom = lambda op: (op_to_desc[op], op)
-
 lexer = ox.make_lexer([
     ('OPENING_BLOCK', r'\('),
     ('CLOSING_BLOCK', r'\)'),
-    ('RIGHT_TAPE', r'right'),
-    ('LEFT_TAPE', r'left'),
-    ('SUM', r'inc|add'),
-    ('SUB', r'dec'),
-    ('PRINT', r'print'),
-    ('READ', r'read'),
     ('NUMBER', r'\d+(\.\d*)?'),
     ('NAME', r'[a-zA-Z_][a-zA-Z_0-9]*'),
 ])
@@ -29,31 +10,21 @@ lexer = ox.make_lexer([
 tokens_list = [
     'OPENING_BLOCK',
     'CLOSING_BLOCK',
-    'RIGHT_TAPE',
-    'LEFT_TAPE',
-    'SUM',
-    'SUB',
-    'PRINT',
-    'READ',
     'NUMBER',
     'NAME',
 ]
 
 parser = ox.make_parser([
-    ('lisp_list : lisp_list atom', treat_list),
-    ('lisp_list : atom', treat_atom),
-    ('atom : NUMBER', lambda number : ('simple_number', float(number))),
-    ('atom : NAME', lambda name : ('simple_name', name)),
-    ('atom : RIGHT_TAPE', treat_atom),
-    ('atom : LEFT_TAPE', treat_atom),
-    ('atom : SUM', treat_atom),
-    ('atom : SUB', treat_atom),
-    ('atom : PRINT', treat_atom),
-    ('atom : READ', treat_atom),
+    ('term : OPENING_BLOCK term CLOSING_BLOCK', lambda opening_block, atom, closing_block: (opening_block, atom, closing_block)),
+    ('term : term atom', lambda term, atom: (term, atom)),
+    ('term : atom term', lambda atom, term: (atom, term)),
+    ('term : atom', lambda term: term),
+    ('atom : NUMBER', lambda x: float(x)),
+    ('atom : NAME', lambda name: name),
 ], tokens_list)
 
 expr = input('expr: ')
 tokens = lexer(expr)
-# ast = parser(tokens)
+ast = parser(tokens)
 print('tokens: ', tokens)
-# print('ast: ', ast)
+print('ast: ', ast)
