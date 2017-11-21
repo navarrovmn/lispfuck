965ff96bbf04eeafc36758251aa97a40e039e03a
Victor Navarro <victor.matias.navarro@gmail.com>
2017-11-21 11:32:32 -0200

    Putting input from a file and ignoring comments
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>

--- lispfuck.py
+++ lispfuck.py
@@ -1,10 +1,34 @@
+from sys import argv
 import ox
+
+file_name, lf_source = argv
+
+in_file = open(lf_source)
+text = in_file.readlines()
+
+def is_comment(line):
+    count = 0
+    while line[count] == ' ':
+        count+=1
+    if line[count] == ';':
+         return True
+    return False
+
+code = ""
+
+for line in text:
+    if is_comment(line):
+         continue
+    code += line.strip('\n')
+
+print(code)
 
 lexer = ox.make_lexer([
     ('OPENING_BLOCK', r'\('),
     ('CLOSING_BLOCK', r'\)'),
     ('NUMBER', r'\d+(\.\d*)?'),
     ('NAME', r'[a-zA-Z_][a-zA-Z_0-9]*'),
+    ('COMMENT', r';')
 ])
 
 tokens_list = [
@@ -12,10 +36,12 @@ tokens_list = [
     'CLOSING_BLOCK',
     'NUMBER',
     'NAME',
+        'COMMENT',
 ]
 
 parser = ox.make_parser([
     ('term : OPENING_BLOCK term CLOSING_BLOCK', lambda opening_block, atom, closing_block: (opening_block, atom, closing_block)),
+    ('term : term term', lambda term, term2: (term, term2)),
     ('term : term atom', lambda term, atom: (term, atom)),
     ('term : atom term', lambda atom, term: (atom, term)),
     ('term : atom', lambda term: term),
@@ -23,8 +49,7 @@ parser = ox.make_parser([
     ('atom : NAME', lambda name: name),
 ], tokens_list)
 
-expr = input('expr: ')
-tokens = lexer(expr)
+tokens = lexer(code)
 ast = parser(tokens)
 print('tokens: ', tokens)
 print('ast: ', ast)
