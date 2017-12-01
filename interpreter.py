8705a4a0bbc4c6e1632252bfadbe43c0993c22e5
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-12-01 20:18:35 -0200

    Add do-before, do-after and read functions
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>

--- interpreter.py
+++ interpreter.py
@@ -13,6 +13,9 @@ class Interpreter:
     def print_cell(self):
         print(chr(self.cells[self.current]))
 
+    def read_cell(self):
+        self.cells[self.current] = int(sys.stdin.read(1))
+
     def inc(self):
         self.cells[self.current] = (self.cells[self.current] + 1)%256
 
@@ -38,24 +41,47 @@ class Interpreter:
             elif isinstance(operation, list):
                 self.eval(operation)
 
+    def do_after(self, args):
+        head, *tail = args
+        another_tail = []
+        for x in tail:
+            if isinstance(x, list):
+                for k in x:
+                    another_tail.append(['do', k, head])
+            else:
+                another_tail.append(x)
+        another_tail.insert(0, 'do')
+        self.eval(another_tail)
+
+    def do_before(self, args):
+        head, *tail = args
+        another_tail = []
+        for x in tail:
+            if isinstance(x, list):
+                for k in x:
+                    another_tail.append(['do', head, k])
+            else:
+                another_tail.append(x)
+        another_tail.insert(0, 'do')
+        self.eval(another_tail)
 
     def eval(self, commands):
         head, *tail = commands
 
         if(head in self.node_to_func):
             func = self.node_to_func[head]
-            self.func()
+            func()
         elif(head in self.node_to_func_with_args):
             func = self.node_to_func_with_args[head]
             func(tail)
-        elif(isinstance(ast, int)):
+        elif(isinstance(head, int)):
             func = self.funcs_with_args.pop()
-            self.func(tail)
+            func(tail)
         else:
             raise ValueError("Maybe you are doing something wrong?")
 
     def __init__(self, ast):
-        self.cells = [0 for x in range(1000)]
+        self.cells = [97 for x in range(1000)]
         self.current = 0
         self.command_list = ast
 
@@ -65,12 +91,21 @@ class Interpreter:
             'inc': self.inc,
             'dec': self.dec,
             'print': self.print_cell,
+            'read': self.read_cell,
         }
 
         self.node_to_func_with_args = {
             'add': self.add,
             'sub': self.sub,
             'do': self.do,
-            'do-before': self.do,
-            'do-after': self.do,
+            'do-before': self.do_before,
+            'do-after': self.do_after,
         }
+
+test = ["do-after", "print", ["inc", "inc"]]
+test2 = ["do", ["add", 2], "print"]
+test3 = ["do-before", "print", ["inc", "inc"]]
+test4 = ["do", "read", ["add", 97], "print"]
+
+interp = Interpreter(test4)
+interp.eval(interp.command_list)
